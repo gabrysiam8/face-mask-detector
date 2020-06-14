74208da2ca7fd2a7a6479c2ab5fb5181f694c937
gabrysiam8 <gabrysiamiedlar@o2.pl>
2020-06-14 22:28:47 +0200

    Extract DataPreparator

--- covid-mask-detector/data_preparation.py
+++ covid-mask-detector/data_preparation.py
@@ -2,32 +2,40 @@ from pathlib import Path
 import pandas as pd
 from tqdm import tqdm
 import cv2
-
-
-# download dataset from link provided by
-# https://github.com/X-zhangyang/Real-World-Masked-Face-Dataset
 from data_download import DataDownloader
 
-downloader = DataDownloader(path_to_save='covid-mask-detector/data/mask.zip', file_id='1UlOk6EtiaXTHylRUx2mySgvJX9ycoeBp')
-downloader.download()
 
-maskDF = pd.DataFrame()
+class DataPreparator:
+    def __init__(self):
+        self.dataframe = pd.DataFrame()
+
+    @staticmethod
+    def download_data(data_path, file_id):
+        downloader = DataDownloader(path_to_save=data_path, file_id=file_id)
+        downloader.download()
+
+    def add_images_to_category(self, category, mask_flag, data_path):
+        for subject in tqdm(list(data_path.iterdir()), unit='files', desc=category):
+            for img_path in subject.iterdir():
+                img = cv2.imread(str(img_path))
+                if img is not None:
+                    self.dataframe = self.dataframe.append({'image': str(img_path), 'mask': mask_flag}, ignore_index=True)
+
+    def save_dataframe(self, dataframe_path):
+        print(f'Saving dataframe to file {dataframe_path}...')
+        self.dataframe.to_pickle(dataframe_path, "infer", 3)
 
 
-def add_photos_to_category(desc, mask_id, maskPath):
-    NoneType = type(None)
-    global subject, imgPath, maskDF
-    for subject in tqdm(list(maskPath.iterdir()), desc=desc):
-        for imgPath in subject.iterdir():
-            img = cv2.imread(str(imgPath))
-            if type(img) != NoneType:
-                maskDF = maskDF.append({'image': str(imgPath), 'mask': mask_id}, ignore_index=True)
+if __name__ == '__main__':
+    data_preparator = DataPreparator()
 
+    # download dataset from link provided by
+    # https://github.com/X-zhangyang/Real-World-Masked-Face-Dataset
+    data_preparator.download_data(data_path='covid-mask-detector/data/mask.zip',
+                                  file_id='1UlOk6EtiaXTHylRUx2mySgvJX9ycoeBp')
 
-datasetPath = Path('covid-mask-detector/data/self-built-masked-face-recognition-dataset')
-add_photos_to_category('mask photos', 1, datasetPath/'AFDB_masked_face_dataset')
-add_photos_to_category('non mask photos', 0, datasetPath/'AFDB_face_dataset')
+    dataset_path = Path('covid-mask-detector/data/self-built-masked-face-recognition-dataset')
+    data_preparator.add_images_to_category('mask photos', 1, dataset_path/'AFDB_masked_face_dataset')
+    data_preparator.add_images_to_category('non mask photos', 0, dataset_path/'AFDB_face_dataset')
 
-dfName = 'covid-mask-detector/data/mask_df.pickle'
-print(f'saving Dataframe to: {dfName}')
-maskDF.to_pickle(dfName, "infer", 3)
+    data_preparator.save_dataframe('covid-mask-detector/data/mask_df.pickle')

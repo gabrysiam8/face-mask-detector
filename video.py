c9b380d9b5ccd635d9edbd5a2e124d28daec5620
Jakub Kowalski <kubabartek1@op.pl>
2020-06-16 14:59:47 +0200

    small change in model

--- video.py
+++ video.py
@@ -4,7 +4,7 @@ import torch
 import pafy
 from torchvision.transforms import Compose, Resize, ToPILImage, ToTensor
 from common.facedetector import FaceDetector
-from train import MaskDetector
+from train import Model
 
 
 @click.command(help="""
@@ -17,7 +17,7 @@ from train import MaskDetector
 def detect(model_path, video_path):
     """ detect if persons in video are wearing masks or not
     """
-    model = MaskDetector()
+    model = Model()
     model.load_state_dict(torch.load(model_path)['state_dict'], strict=False)
     
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -40,8 +40,6 @@ def detect(model_path, video_path):
     labels = ['Without mask', 'With mask']
     label_color = [(0, 0, 255), (0, 255, 0)]
 
-    # vPafy = pafy.new(video_path)
-    # play = vPafy.getbest(preftype="mp4")
     vc = cv2.VideoCapture(video_path)
 
     while True:
@@ -50,18 +48,14 @@ def detect(model_path, video_path):
         for face in faces:
             (x, y, w, h) = face
             
-            # clamp coordinates that are outside of the image
             x, y = max(x, 0), max(y, 0)
             
-            # predict mask label on extracted face
             face_img = frame[y:y+h, x:x+w]
             output = model(transformations(face_img).unsqueeze(0).to(device))
             _, predicted = torch.max(output.data, 1)
             
-            # draw face frame
             cv2.rectangle(frame, (x, y), (x+w, y+h), label_color[predicted], 2)
             
-            # draw prediction label
             cv2.putText(frame, labels[predicted], (x, y-20), font, 0.6, label_color[predicted], 2)
 
         cv2.imshow('detector', frame)
